9c5db78d94ec882d44584994a7c58d785bff923a
Sofyan <nkeman200@gmail.com>
2026-03-26 18:36:32 +0700

    Auto insert admin user (Seeding) hapus

--- src/main/java/com/kampus/sepedaweb/SepedawebApplication.java
+++ src/main/java/com/kampus/sepedaweb/SepedawebApplication.java
@@ -14,22 +14,22 @@ public class SepedawebApplication {
     }
 
     // Fungsi ini akan otomatis berjalan satu kali setiap server menyala
-    @Bean
-    public CommandLineRunner insertAdmin(JdbcTemplate jdbcTemplate) {
-        return args -> {
-            try {
-                // 1. Bersihkan user lama yang cacat
-                jdbcTemplate.execute("DELETE FROM users WHERE username = 'sss'");
+    // @Bean
+    // public CommandLineRunner insertAdmin(JdbcTemplate jdbcTemplate) {
+    //     return args -> {
+    //         try {
+    //             // 1. Bersihkan user lama yang cacat
+    //             jdbcTemplate.execute("DELETE FROM users WHERE username = 'sss'");
                 
-                // 2. Tembakkan user baru dari dalam sistem
-                jdbcTemplate.execute("INSERT INTO users (id, username, password, role, kode_pegawai) VALUES (99, 'sss', '$2a$10$JOUj3w1eG4DsGFBa45vawuo3c2dRy9o1U78l8J7V0jjKtOyxbJ6O6', 'admin', 1212)");
+    //             // 2. Tembakkan user baru dari dalam sistem
+    //             jdbcTemplate.execute("INSERT INTO users (id, username, password, role, kode_pegawai) VALUES (99, 'sss', '$2a$10$JOUj3w1eG4DsGFBa45vawuo3c2dRy9o1U78l8J7V0jjKtOyxbJ6O6', 'admin', 1212)");
                 
-                System.out.println("=========================================");
-                System.out.println("✅ BERHASIL: AKUN ADMIN SUDAH DIBUAT!");
-                System.out.println("=========================================");
-            } catch (Exception e) {
-                System.out.println("Gagal membuat akun: " + e.getMessage());
-            }
-        };
-    }
+    //             System.out.println("=========================================");
+    //             System.out.println("✅ BERHASIL: AKUN ADMIN SUDAH DIBUAT!");
+    //             System.out.println("=========================================");
+    //         } catch (Exception e) {
+    //             System.out.println("Gagal membuat akun: " + e.getMessage());
+    //         }
+    //     };
+    // }
 }
